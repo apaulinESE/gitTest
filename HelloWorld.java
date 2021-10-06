ac90e440ac936c214b2736d6ca7084adace05d33
Andri Paulin <andri.paulin@students.unibe.ch>
2021-10-06 20:51:37 +0200

    added the countToTen()-method

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,17 @@ public class HelloWorld
 {
     public static void main(String[] args) {
         System.out.println("Now it does Work! Neat!");
+        countToTen();
+    }
+
+    /**
+     * A method to print numbers from one to ten.
+     */
+    private static void countToTen(){
+
+        for (int i = 0; i <= 10; i++){
+            System.out.println(i);
+        }
+        return;
     }
 }
